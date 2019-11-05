a28d16ce2fd269dd726d63263ee77ff79e7d340f
Nikheel001 <nikheel.patel0007@gmail.com>
2019-11-05 20:48:41 +0530

    added getters and setters in Person Class

--- src/app/people/person.ts
+++ src/app/people/person.ts
@@ -4,4 +4,36 @@ class Person
   private __id:String;
   private __owes:Map<String, Number>;
   private __owed:Number;
+
+  public setOwes(owes: Map<String, Number>):void {
+    this.__owes = owes;
+  }
+
+  public getOwes():Map<String, Number> {
+    return this.__owes;
+  }
+
+  public setOwed(owed: Number):void{
+    this.__owed = owed;
+  }
+
+  public getOwed():Number {
+    return this.__owed;
+  }
+
+  public setId(id :String ):void {
+    this.__id = id;
+  }
+
+  public getId():String{
+    return this.__id;
+  }
+
+  public setName(name:String):void {
+    this.__name = name;
+  }
+
+  public getName():String{
+    return this.__name;
+  }
 }
