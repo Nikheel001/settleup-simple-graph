0a6fd2276fcfc292a8f4f61d23f6c07195232981
Nikheel001 <nikheel.patel0007@gmail.com>
2019-11-17 21:30:21 +0530

    user and graph model with methods added

---
+++ src/app/models/Graph.ts
@@ -0,0 +1,124 @@
+import { User } from ".\User";
+
+export class Graph {
+  private _value: Map<String, User>;
+  private _id: String;
+  private _name: String;
+  private _visit_it: Set<String>;
+
+  constructor() {
+    this._id = "";
+    this._name = "";
+    this._value = new Map<String, User>();
+    this._visit_it = new Set<String>();
+  }
+
+  public get name(): String {
+    return this._name;
+  }
+
+  public set name(newName: String) {
+    this._name = newName;
+  }
+
+  public get value(): Map<String, User> {
+    return this._value;
+  }
+
+  public set value(newValue) {
+    newValue.forEach((i, j) => {
+      if (!(j instanceof User)) {
+        console.log("Invalid Value");
+        return;
+      }
+    });
+    this._value = newValue;
+  }
+
+  public set Id(Id: String) {
+    this._id = Id;
+  }
+
+  public get Id(): String {
+    return this._id;
+  }
+
+  public transaction(x, y, money) {
+    let i: User = this.value.get(x);
+    let M: Number = i.Owes.get(y);
+    M += money;
+    i.Owes.set(y, M);
+  }
+
+  public process(x, y, Money) {
+    if (!this.value.has(y)) {
+      return;
+    }
+    let ychd: Set<String> = this.value.get(y).owes_to();
+
+    while (ychd.size > 0) {
+      ychd.forEach(yc => {
+        let amt = this.value.get(y).owedMoney(x);
+        if (Money > amt) {
+          let tmp = Money;
+          Money = amt;
+          amt = tmp;
+        }
+        this.value.get(x).pay(y, Money);
+        this.value.get(y).pay(yc, Money);
+        if (yc != x) {
+          this.transaction(x, yc, Money);
+          if (!this._visit_it.has(yc)) {
+            this._visit_it.add(yc);
+          }
+        }
+      });
+    }
+  }
+
+  public showNetwork() {
+    console.log(this.value);
+  }
+
+  public clearGraph() {
+    let nodes = new Set(this._value.keys());
+    let marked = new Set<String>();
+    nodes.forEach(i => {
+      if (this._value.get(i).size() == 0) {
+        marked.add(i);
+      }
+    });
+
+    marked.forEach(m => {
+      nodes.delete(m);
+    });
+  }
+
+  public static fromDict(graph) {
+    let x = new Graph();
+    x.name = graph.get("name");
+    x.value = graph.get("value");
+    x.Id = graph.get("_id");
+    return x;
+  }
+
+  public graphAsDict() {
+    let view = new Map<String, any>();
+    view.set("name", this.name);
+    view.set("value", this.value);
+  }
+
+  public processAll() {
+    this._visit_it = new Set<String>(this.value.keys());
+    this._visit_it.forEach(k => {
+      if (this._visit_it.has(k)) {
+        this.value
+          .get(k)
+          .owes_to()
+          .forEach(i => {
+            this.process(k, i, this.value.get(k).owedMoney(i));
+          });
+      }
+    });
+  }
+}
---
+++ src/app/models/User.ts
@@ -0,0 +1,115 @@
+export class User {
+  private __name: String;
+  private __id: String;
+  private __owes: Map<String, number>;
+  private __owed: number;
+  private __cellNo: String;
+
+  public set Owes(owes: Map<String, number>) {
+    this.__owes = owes;
+  }
+
+  public get Owes(): Map<String, number> {
+    return this.__owes;
+  }
+
+  public set Owed(owed: number) {
+    this.__owed = owed;
+  }
+
+  public get Owed(): number {
+    return this.__owed;
+  }
+
+  public set ObjectId(id: String) {
+    this.__id = id;
+  }
+
+  public get ObjectId(): String {
+    return this.__id;
+  }
+
+  public set Name(name: String) {
+    this.__name = name;
+  }
+
+  public get Name(): String {
+    return this.__name;
+  }
+
+  public get CellNo(): String {
+    return this.__cellNo;
+  }
+
+  public set CellNo(no: String) {
+    this.__cellNo = no;
+  }
+
+  public toString(): String {
+    let owes_to: String = "";
+    this.__owes.forEach((money: number, to: String) => {
+      owes_to += " to : " + to + "Money : " + money + "\n";
+    });
+
+    return (
+      "Name : " +
+      this.__name +
+      "\n" +
+      "Money owed : " +
+      this.__owed +
+      "\n" +
+      "Money owes to: \n" +
+      owes_to
+    );
+  }
+
+  public owes_to(): Set<String> {
+    return new Set(this.__owes.keys());
+  }
+
+  public owedMoney(to: String): number {
+    return this.Owes.get(to);
+  }
+
+  public pay(to, Money: number) {
+    let M: number = this.__owes.get(to);
+    M = M - Money;
+    this.__owes.set(to, M);
+    if (M == 0) {
+      this.__owes.delete(to);
+    }
+  }
+
+  public size(): number {
+    return this.__owes.size;
+  }
+
+  public userAsDict() {
+    let view = new Map<String, any>();
+    view.set("name", this.Name);
+    view.set("owed", this.Owed);
+    view.set("owes", this.Owes);
+    view.set("CellNumber", this.CellNo);
+    return view;
+  }
+
+  /*
+  verify weather no already exists in db
+  pin verification from user's cell
+  returns true or false
+  */
+  public static verifyCellNo(no): boolean {
+    return true;
+  }
+
+  public static fromDict(usr: Map<String, any>) {
+    let x: User = new User();
+
+    x.ObjectId = usr.get("_id");
+    x.Name = usr.get("name");
+    x.Owed = usr.get("owed");
+    x.Owes = usr.get("owes");
+    x.CellNo = usr.get("no");
+    return x;
+  }
+}
